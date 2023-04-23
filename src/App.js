dcb5966674f4b3a726a621691bec5e80fcc1e8bf
alanbacay02 <alan.social02@gmail.com>
2023-04-23 17:19:06 +0800

    Adds initial sound pad component and buttons

--- src/App.js
+++ src/App.js
@@ -1,11 +1,52 @@
 import './App.css';
+//import { useState } from 'react';
+import PropTypes from 'prop-types';
 
-function App() {
+function SoundButton({ handleClick, name }) {
 	return (
-		<div className="App">
-			<h1 className="text-3xl">Hello!</h1>
+		<div>
+			<button 
+				onClick={handleClick}
+				className="h-14 w-16 rounded-md text-white bg-gray-400"
+			>{name}</button>
 		</div>
 	);
 }
+SoundButton.propTypes = {
+	handleClick: PropTypes.func.isRequired,
+	name: PropTypes.string
+};
 
-export default App;
+function SoundPad({ handleClick }) {
+	const buttonNames = ['Q', 'W', 'E', 'A', 'S', 'D', 'Z', 'X', 'C'];
+
+	const soundButtonArr = buttonNames.map((name, index) => {
+		return (
+			<SoundButton key={index} handleClick={handleClick} name={name}/>
+		);
+	});
+
+	return (
+		<div className="grid grid-cols-3 gap-2 max-w-[290px] mx-auto p-4">
+			{soundButtonArr}
+		</div>
+	);
+}
+SoundPad.propTypes = {
+	handleClick: PropTypes.func.isRequired
+};
+
+export default function App() {
+	const audio = new Audio('./soundpad/Heater-1.mp3');
+
+	function handleClick() {
+		audio.currentTime = 0;
+		audio.play();
+	}
+
+	return (
+		<div className="App">
+			<SoundPad handleClick={handleClick} />
+		</div>
+	);
+}
