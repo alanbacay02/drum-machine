3f1e4eb1c60b140e382551f768bccbce099a1d22
alanbacay02 <alan.social02@gmail.com>
2023-05-04 00:49:28 +0800

    Imports useRef to handle button UI change on keydown event.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import PropTypes from 'prop-types';
 
 // Creates an array to store audio file paths and instrument names.
@@ -15,45 +15,47 @@ const AUDIO_FILES = [
 	{ instName: 'Closed High-hat', fileName: './soundpad/Cev_H2.mp3' }
 ];
 
-// Creates a `SoundButton` component with passed in props `handleClick` which is a function, `name` which is a string, `id` which is an string, and `isActive` which is a bool.
-function SoundButton({ handleClick, name, id, isActive }) {
+// Creates a `SoundButton` component with passed in props `handleClick` which is a function, `name` which is a string, `id` which is an string, `isActive` which is a bool, and `buttonRefs` which is a function.
+function SoundButton({ handleClick, name, id, buttonRefs}) {
 	return (
 		// Returns a <div> with a <button> element inside it.
 		<div className="m-0 p-0">
 			<button 
+				ref={buttonRefs}
 				id={id}
 				onClick={handleClick}
-				className={`h-[88px] w-[88px] rounded-2xl focus:outline-none active:bg-red-500 text-white ${isActive ? 'bg-red-500' : 'bg-sky-950'}`}
+				className={'h-[88px] w-[88px] rounded-2xl focus:outline-none bg-slate-800 transition-colors duration-100 text-white active:bg-red-500'}
 			>{name}</button>
 		</div>
 	);
 }
-// Validates props for `SoundButton` where `handleClick` is required a function, `name` is a string, `id` is required a string, and `isActive` is required a boolean value.
+// Validates props for `SoundButton` where `handleClick` is required a function, `name` is a string, `id` is required a string, `isActive` is required a boolean value, and `buttonRefs` is a function.
 SoundButton.propTypes = {
 	handleClick: PropTypes.func.isRequired,
 	name: PropTypes.string,
 	id: PropTypes.string.isRequired,
-	isActive: PropTypes.bool.isRequired
+	isActive: PropTypes.bool,
+	buttonRefs: PropTypes.func
 };
 
-// Creates a component `SoundPad` with passed in props `handleClick` which is a function, `activeButtonIndex` which is an integer, and `instName` which is a string.
-function SoundPad({ handleClick, activeButtonIndex, instName }) {
+// Creates a component `SoundPad` with passed in props `handleClick` which is a function, `activeButtonIndex` which is an integer, `instName` which is a string, and `buttonRefs` which is a object.
+function SoundPad({ handleClick, instName, buttonRefs}) {
 	// Creates an array `buttonNames` used to display text for each of our buttons.
 	const buttonNames = ['Q', 'W', 'E', 'A', 'S', 'D', 'Z', 'X', 'C'];
-	// Creates a new array `soundButtonArr` by mapping over `buttonNames` array.
-	const soundButtonArr = buttonNames.map((name, index) => {
+	// Creates a new array `soundButtonArray` by mapping over `buttonNames` array.
+	const soundButtonArray = buttonNames.map((name, index) => {
 		// Assigns a boolean value to `isActive` depending if `activeButtonIndex` and `index` are equal to each other.
 		return (
 			// Returns a child element `SoundButton` with a unique id, key, and name assigned to it. Props `handleClick` and `isActive` is also passed to it.
-			<SoundButton id={`soundButton${index}`} key={index} name={name} handleClick={() => {handleClick(index);}} isActive={activeButtonIndex}/>
+			<SoundButton buttonRefs={(el) => (buttonRefs.current[index] = el)} id={`soundButton${index}`} key={index} name={name} handleClick={() => {handleClick(index);}}/>
 		);
 	});
 
 	return (
-		// Returns a <div> with an array `soundButtonArr`.
+		// Returns a <div> with an array `soundButtonArray`.
 		<div className="flex flex-col gap-y-4 max-w-[300px] py-4 mx-auto mt-28 bg-blue-400 rounded-xl">
 			<div className="grid grid-cols-3 gap-[6px] mx-auto">
-				{soundButtonArr}
+				{soundButtonArray}
 			</div>
 			<div className="flex flex-row justify-center">
 				{/* Include Text Area Here!*/}
@@ -70,16 +72,16 @@ function SoundPad({ handleClick, activeButtonIndex, instName }) {
 		</div>
 	);
 }
-// Validates props for `SoundPad` where `handleClick` is required a function, `activeButtonIndex` is a number, and `instName` is a string.
+// Validates props for `SoundPad` where `handleClick` is required a function, `activeButtonIndex` is a number, `instName` is a string, and `buttonRefs` which is an object.
 SoundPad.propTypes = {
 	handleClick: PropTypes.func.isRequired,
 	activeButtonIndex: PropTypes.number,
-	instName: PropTypes.string
+	instName: PropTypes.string,
+	buttonRefs: PropTypes.object.isRequired
 };
 
 export default function App() {
-	// Creates state `activeButtonIndex` to track whenever a button is pressed or not.
-	const [activeButtonIndex, setActiveButtonIndex] = useState(false);
+	const buttonRefs = useRef([]);
 	// Creates state `instName` to track name of instrument being played.
 	const [instName, setInstName] = useState('');
 	// Maps over `AUDIO_FILES` array and creates new audio object using `new Audio` constructor.
@@ -96,21 +98,28 @@ export default function App() {
 		'x': 7,
 		'c': 8
 	};
-	// const audioKeys = ['q','w','e','a','s','d','z','x','c'];
 
 	// Creates an Event listener on app initialization that will be used to track keydown events.
 	useEffect(() => {
+		// Creates an object `pressedKeys` to track key events.
 		const pressedKeys = {}; 
 		// Adds an event listener to the document that listens for keydown events.
 		const handleKeyDown = (event) => {
+			// Gets the event key and assigns it to `key`.
 			const key = event.key.toLowerCase();
+			// Adds the key to `pressedKeys` with its respective index from `audioMap` as its value.
 			pressedKeys[key] = audioMap[key];
+			// Calls the `playSounds` function with the `pressedKeys` object.
 			playSounds(pressedKeys);
 		};
+		// Adds an event listener to the document that listens for keydup events.
 		const handleKeyUp = (event) => {
+			// Gets the event key and assigns it to `key`.
 			const key = event.key.toLowerCase();
+			// Deletes the key from the `pressedKeys` object.
 			delete pressedKeys[key];
 		};
+		// Adds `keydown` and `keyup` event listeners to the document where functions `handleKeyDown` and `handleKeyUp` are triggered when their respective events occur.
 		document.addEventListener('keydown', handleKeyDown);
 		document.addEventListener('keydown', handleKeyUp);
 		return () => {
@@ -118,37 +127,58 @@ export default function App() {
 			document.removeEventListener('keydown', handleKeyDown);
 			document.removeEventListener('keyup', handleKeyUp);
 		};
+	}, []);
 
-	}, [activeButtonIndex]);
-
+	// Helper function that plays audio files based on the keys stored in `pressedKeys`.
 	function playSounds(pressedKeys) {
+		// Iterates through the `pressedKeys` object
 		for (let key in pressedKeys) {
+			// Checks if `audioMap` object has the key from `pressedKeys`.
 			if (Object.prototype.hasOwnProperty.call(audioMap, key)) {
-				audio[pressedKeys[key]].currentTime = 0;
-				audio[pressedKeys[key]].play();
-				let buttonTarget = document.getElementById(`soundButton${pressedKeys[key]}`);
-				buttonTarget.classList.add('active');
-				setTimeout(() => {
-					buttonTarget.classList.remove('active');
-				}, 100);
+				// Stores the integer value of the `key` in `pressedKeys` to `keyIndex`.
+				let keyIndex = pressedKeys[key];
+				// Calls `playAudio` to play the respective audio file based on the value supplied by `keyIndex`.
+				playAudio(keyIndex);
+				// Calls `activateButton` to handle active button states.
+				activateButton(keyIndex);
+				// Sets `instName` state to respective `instName` of audio file from `AUDIO_FILES`.
+				setInstName(AUDIO_FILES[keyIndex].instName);
 			}
 		}
 	}
 	
 	// Creates a function that will play audio when a sound button is clicked.
 	function handleClick(index) {
-		// Gets the audio file being played and resets its play duration to 0.
-		audio[index].currentTime = 0;
-		// Plays the audio file located in the `audio` array associated with the index.
-		audio[index].play();
+		// Calls `playAudio` to play the respective audio file based on the value supplied by `index`.
+		playAudio(index);
 		// Sets `instName` state to name of instrument being played.
 		setInstName(AUDIO_FILES[index]['instName']);
 	}
 
-	// Returns a div `App` with child element `SoundPad` with passed in props `handleClick` which is a function, `activeButtonIndex` which is an integer and `instName` which is a string.
+	// Helper function `playAudio` that playes a respective audio file based on the supplied `index`.
+	function playAudio(audioIndex) {
+		// Gets the audio file being played and resets its play duration to 0.
+		audio[audioIndex].currentTime = 0;
+		// Plays the audio file located in the `audio` array associated with the index.
+		audio[audioIndex].play();
+	}
+
+	// Helper function that updates the references of the buttons.
+	function activateButton(buttonIndex) {
+		// Gets the reference of the button targeted.
+		const buttonTarget = buttonRefs.current[buttonIndex];
+		// Adds an `active` attribute to the `className` of the button.
+		buttonTarget.classList.add('active');
+		setTimeout(() => {
+			// Removes the `active` attribute from `className` after 100ms.
+			buttonTarget.classList.remove('active');
+		}, 150);
+	}
+
+	// Returns a div `App` with child element `SoundPad` with passed in props `handleClick` which is a function, `activeButtonIndex` which is an integer, `instName` which is a string, and `buttonRefs` which is an object.
 	return (
 		<div className="App">
-			<SoundPad handleClick={handleClick} activeButtonIndex={activeButtonIndex} instName={instName}/>
+			<SoundPad buttonRefs={buttonRefs} handleClick={handleClick} instName={instName}/>
 		</div>
 	);
 }
